c93ae4f3923db72559ffea0131ae76e0656d9d71
Maciej Sikora <maciej.sikora.1999@wp.pl>
2023-07-20 21:32:05 +0200

    end hw 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,9 +2,6 @@ import React, { Component } from 'react';
 import { Phonebook } from './Phonebook/Phonebook';
 
 export class App extends Component {
-  constructor() {
-    super();
-  }
   render() {
     return (
       <div className="app">
